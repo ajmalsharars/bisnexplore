4b71b06b8d9c0c4c557eef3fbc851080b7ef4bb6
ajmalsharars <146179851+ajmalsharars@users.noreply.github.com>
2023-10-03 21:52:05 +0430

    Re-design of About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,66 +2,56 @@ import coverPhoto from "../../../public/images/about.jpg";
 import Image from "next/image";
 async function page() {
   return (
-    <div className="bg-gray-50">
-      <section
-        className="bg-center bg-no-repeat bg-gray-600 bg-blend-multiply"
-        style={{ backgroundImage: `url(${coverPhoto.src})` }}
-      >
-        <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
-          <h1 className="mb-4 text-2xl font-semibold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
-            About us
-          </h1>
-          <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">
-            Welcome to Bisnexplore, the world’s first platform where companies
-            connect with each other word wide to amplify their businesses.
-          </p>
-          <hr className="w-48 h-1 mx-auto my-4 bg-gray-50 border-0 rounded md:my-10"></hr>
+    <div className="">
+      <section className="bg-center bg-no-repeat bg-[url('')] bg-gray-100 bg-blend-multiply">
+        <div className="px-4 mx-auto max-w-screen-xl text-center py-10 lg:py-24">
+          <div>
+            <h1 className="mb-8 text-4xl font-extrabold leading-8 tracking-tight  text-blue-950 md:text-4xl lg:text-5xl">
+              About us{" "}
+            </h1>
+            <hr className="w-48 h-1 mx-auto my-4 bg-blue-950 border-0 rounded md:my-10"></hr>
+            <p className="mb-4 font-extralight   text-gray-600 lg:text-xl sm:px-16 lg:px-48">
+              Welcome to Bisnexplore, the world’s first platform where companies
+              connect with each other word wide to amplify their businesses.
+            </p>
+            <hr className="border-1 border-gray-200 mb-1 mx-auto drop-shadow-sm" />
+          </div>
         </div>
       </section>
-      {/* Start of Cards */}
-      <div className="bg-gray-50 flex flex-wrap gap-6 justify-center items-center mb-10 mt-10 pt-10 pb-10">
-        <div className="w-full max-w-xs px-8 py-4 items-center bg-white rounded-2xl shadow-lg">
-          <div className="flex justify-center  md:justify-end"></div>
+      {/* Start of other section */}
 
-          <h2 className="mt-2 text-xl font-semibold  md:mt-0 text-blue-600">
+      <div>
+        <div className="w-full mx-auto my-10 justify-center items-center">
+          <h1 className="text-fuchsia-900 uppercase text-2xl text-center py-10">
             Our vision
-          </h2>
-
-          <p className="mt-2 mb-10 text-sm text-gray-600">
+          </h1>
+          <h1 className="pl-8 pb-28 mb-8  text-2xl font-extralight  leading-8 tracking-tight  text-blue-950 md:text-xl lg:text-2xl">
             The vision of Bisnexplore is to become the global catalyst for
             seamless business connections, driving innovation and growth in
             emerging markets and technology sectors.
-          </p>
+          </h1>
         </div>
 
-        <div className="w-full max-w-xs px-8 py-4 items-center bg-white rounded-2xl shadow-lg">
-          <div className="flex justify-center  md:justify-end"></div>
-
-          <h2 className="mt-2 text-xl font-semibold  md:mt-0 text-blue-600">
+        <div className="w-full mx-auto my-10 justify-center items-center bg-slate-100 py-10">
+          <h1 className="text-fuchsia-900 uppercase text-2xl text-center py-10">
             Our mission
-          </h2>
-
-          <p className="mt-2 mb-10 text-sm text-gray-600">
+          </h1>
+          <h1 className="pl-8 pb-28 mb-8  text-2xl font-extralight  leading-8 tracking-tight  text-blue-950 md:text-xl lg:text-2xl">
             Our mission is to empower businesses, facilitating connections that
             ignite progress in emerging markets and technologyg
-          </p>
-          <br />
+          </h1>
         </div>
 
-        <div className="w-full max-w-xs px-8 py-4 items-center bg-white rounded-2xl shadow-lg">
-          <h2 className="mt-2 text-xl font-semibold md:mt-0 text-blue-600">
+        <div className="w-full mx-auto my-10 justify-center items-center">
+          <h1 className="text-fuchsia-900 uppercase text-2xl text-center py-10">
             Who we are
-          </h2>
-
-          <p className="mt-2 mb-10 text-sm text-gray-600">
+          </h1>
+          <h1 className="pl-8 pb-28 mb-8  text-2xl font-extralight  leading-8 tracking-tight  text-blue-950 md:text-xl lg:text-2xl">
             There should be some text...... There should be some text......
             There should be some text......
-          </p>
-          <br />
-          <br />
+          </h1>
         </div>
       </div>
-      ___
     </div>
   );
 }
